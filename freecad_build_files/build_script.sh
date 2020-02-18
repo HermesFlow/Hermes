3bb2663397651b0d670c7128446a5035cb519891
Yakov Mindelis <jack@iscfdc.co.il>
2020-02-18 20:41:05 +0200

    bashrc replacement and docker script that use them via binding

--- freecad_build_files/build_script.sh
+++ freecad_build_files/build_script.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-( apt-get update && apt-get install libqt5webkit5-dev)
-set -e
+
+set -x
 
 cmake -j$(nproc) \
     -D BOOST_ROOT=/usr/local/include/boost \
-    -D BUILD_QT5=ON -DBUILD_FEM=OFF -DBUILD_SANDBOX=ON \
-    -D PYTHON_LIBRARY=/usr/local/lib/libpython3.7.a \
+    -D BUILD_QT5=ON -D BUILD_FEM=ON -D BUILD_SANDBOX=ON \
+    -D PYTHON_LIBRARY=/usr/local/lib/libpython3.7m.so \
     -D PYTHON_INCLUDE_DIR=/usr/local/include/python3.7 \
     -D PYTHON_PACKAGES_PATH=/usr/local/lib/python3.7/site-packages \
     -D PYTHON_EXECUTABLE=/usr/local/bin/python3 \
     -D SHIBOKEN_INCLUDE_DIR=/usr/local/lib/python3.7/site-packages/shiboken2_generator/include \
-    -D SHIBOKEN_LIBRARY=/usr/local/lib/python3.7/\
-site-packages/shiboken2/libshiboken2.cpython-37-x86_64-linux-gnu.so.5.12 \
+    -D SHIBOKEN_LIBRARY=/usr/local/lib/python3.7/site-packages/shiboken2/libshiboken2.cpython-37m-x86_64-linux-gnu.so.5 \
     -D PYSIDE_INCLUDE_DIR=/usr/local/lib/python3.7/site-packages/PySide2/include \
-    -D PYSIDE_LIBRARY=/usr/local/lib/python3.7/site-packages\
-/PySide2/libpyside2.cpython-37-x86_64-linux-gnu.so.5.12 \
+    -D PYSIDE_LIBRARY=/usr/local/lib/python3.7/site-packages/PySide2/libpyside2.cpython-37m-x86_64-linux-gnu.so.5 \
     -D PYSIDE2RCCBINARY=/usr/local/lib/python3.7/site-packages/PySide2/pyside2-rcc \
     -D FREECAD_USE_QTWEBMODULE="Qt\ Webkit" \
     -S /mnt/source \
@@ -23,7 +21,3 @@ site-packages/shiboken2/libshiboken2.cpython-37-x86_64-linux-gnu.so.5.12 \
 cd /mnt/build
 
 make -j $(nproc)
-
-# Note, had to add this to freecad source CMakeLists.txt:
-# add_compile_options(-fpermissive -fPIC)
-
--- freecad_build_files/docker_dev.sh
+++ freecad_build_files/docker_dev.sh
@@ -2,10 +2,13 @@
 
 fc_source="$PWD/source"
 fc_build="$PWD/build"
+fc_build_files="$PWD/pyHermes/freecad_build_files/"
 
-docker run -it --rm \
+sudo docker run -it --rm \
 -v $fc_source:/mnt/source \
 -v $fc_build:/mnt/build \
+-v $fc_build_files:/mnt/build_files \
+-v $fc_build_files/bashrc_dev:/root/.bashrc:ro \
 -v $other_files:/mnt/files \
 -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 registry.gitlab.com/daviddaish/freecad_docker_env:latest
