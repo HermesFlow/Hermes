66fe3edfa94b99e5a01e7b83184dc221ddc443be
Yakov Mindelis <jack@iscfdc.co.il>
2020-02-20 23:52:24 +0200

    dot_local is pip3 PyQt5, docker scripts mods

--- freecad_build_files/docker.sh
+++ freecad_build_files/docker.sh
@@ -5,6 +5,8 @@ fc_build="$PWD/build"
 docker run -it --rm \
 -v $fc_build:/mnt/build \
 -v $other_files:/mnt/files \
+-v $fc_build_files/bashrc:/root/.bashrc:ro \
+-v $fc_build_files/dot_local:/root/.local:ro \
 -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 registry.gitlab.com/daviddaish/freecad_docker_env:latest
 
--- freecad_build_files/docker_dev.sh
+++ freecad_build_files/docker_dev.sh
@@ -9,6 +9,7 @@ sudo docker run -it --rm \
 -v $fc_build:/mnt/build \
 -v $fc_build_files:/mnt/build_files \
 -v $fc_build_files/bashrc_dev:/root/.bashrc:ro \
+-v $fc_build_files/dot_local:/root/.local:ro \
 -v $other_files:/mnt/files \
 -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 registry.gitlab.com/daviddaish/freecad_docker_env:latest
