1fddd41a92f4d2ff3d42623b5085c65131916467
Yakov Mindelis <jack@iscfdc.co.il>
2020-02-24 18:53:48 +0200

    sanitation and cleanup of docker scripts

--- freecad_build_files/docker.sh
+++ freecad_build_files/docker.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-fc_build="$PWD/build"
+wd=`basename $0`
+fc_build="$wd/build"
 
 docker run -it --rm \
--v $fc_build:/mnt/build \
--v $other_files:/mnt/files \
--v $fc_build_files/bashrc:/root/.bashrc:ro \
--v $fc_build_files/dot_local:/root/.local:ro \
+-v "$fc_build":/mnt/build \
+-v "$other_files":/mnt/files \
+-v "$fc_build_files/bashrc":/root/.bashrc:ro \
+-v "$wd/dot_local":/root/.local:ro \
 -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 registry.gitlab.com/daviddaish/freecad_docker_env:latest
 
--- freecad_build_files/docker_dev.sh
+++ freecad_build_files/docker_dev.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-fc_source="$PWD/source"
-fc_build="$PWD/build"
+wd=`basename $0`
+fc_source="$wd/source"
+fc_build="$wd/build"
 fc_build_files="$PWD/pyHermes/freecad_build_files/"
 
 sudo docker run -it --rm \
--v $fc_source:/mnt/source \
--v $fc_build:/mnt/build \
--v $fc_build_files:/mnt/build_files \
--v $fc_build_files/bashrc_dev:/root/.bashrc:ro \
--v $fc_build_files/dot_local:/root/.local:ro \
+-v "$fc_source:/mnt/source \
+-v "$fc_build:/mnt/build \
+-v "$fc_build_files:/mnt/build_files \
+-v "$fc_build_files/bashrc_dev:/root/.bashrc:ro \
+-v "$wd/dot_local:/root/.local:ro \
 -v $other_files:/mnt/files \
 -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 registry.gitlab.com/daviddaish/freecad_docker_env:latest
