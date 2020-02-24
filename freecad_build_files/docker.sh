f3613a9b37ce1314670690568e93f35a5c8b331d
Yakov Mindelis <jack@iscfdc.co.il>
2020-02-24 20:15:25 +0200

    adding jsonpath-rw-ext to ./local

--- freecad_build_files/docker.sh
+++ freecad_build_files/docker.sh
@@ -13,7 +13,7 @@ docker run -it --rm \
 -v "$fc_examples":/mnt/examples \
 -v "$fc_build_files/bashrc":/root/.bashrc:ro \
 -v "$other_files":/mnt/files \
--v "$wd/dot_local":/root/.local:rw \
+-v "$wd/dot_local":/root/.local:ro \
 -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 registry.gitlab.com/daviddaish/freecad_docker_env:latest
 
