622c22c5b55f830cbfd240736f3a2d09fa0dd9d5
Noga <noga@iscfdc.com>
2020-04-21 10:04:10 +0300

    add new structure for openFOAM executer

---
+++ hermes/Resources/executers/programs/openFOAM/openFOAM.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+class controlDict:
+    '''
+        will transform controDict from JSON structure
+        to openFOAM structure   '''
+
+    def run(self, **inputs):
+        ''' will use jinja template located in system.controlDict.py  ? '''
+        return
+    
