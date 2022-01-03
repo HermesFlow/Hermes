2dcc958405c40a13b084f31bd9e6e16fde44ae78
yehudaa <yehudaa@iibr.gov.il>
2022-01-03 16:49:00 +0200

    Fixed the jinja templates of the dispersion

--- hermes/Resources/executers/openFOAM/dispersionExecuters.py
+++ hermes/Resources/executers/openFOAM/dispersionExecuters.py
@@ -7,6 +7,6 @@ import re
 class kinematicCloudPropertiesExecuter(jinjaExecuter):
 
     def run(self, **inputs):
-        template = self._getTemplate("openFOAM/kinematicCloudProperties/kinematicCloudProperties")
+        template = self._getTemplate("openFOAM/dispersion/kinematicCloudProperties")
         output = template.render(**inputs)
         return dict(openFOAMfile=output)
