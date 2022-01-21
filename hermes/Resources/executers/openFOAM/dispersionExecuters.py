c236d938c212da63faf35b0c7e369ca166f709bd
yehudaa <yehudaa@iibr.gov.il>
2022-01-21 10:15:11 +0200

    Additional changes to the dispersin and changes the workflow-bin command names

--- hermes/Resources/executers/openFOAM/dispersionExecuters.py
+++ hermes/Resources/executers/openFOAM/dispersionExecuters.py
@@ -10,3 +10,13 @@ class kinematicCloudPropertiesExecuter(jinjaExecuter):
         template = self._getTemplate("openFOAM/dispersion/kinematicCloudProperties")
         output = template.render(**inputs)
         return dict(openFOAMfile=output)
+
+
+
+
+class IndoorDictExecuter(jinjaExecuter):
+
+    def run(self, **inputs):
+        template = self._getTemplate("openFOAM/dispersion/stochasticDispersion/IndoorDict")
+        output = template.render(**inputs)
+        return dict(openFOAMfile=output)
