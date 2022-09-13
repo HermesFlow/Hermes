0b65df57cfdbfcd7f427a56093ef634d8e9b9296
yehudaa <yehudaa@iibr.gov.il>
2022-09-13 15:43:58 +0300

    Finished the changeDictionary

---
+++ hermes/Resources/openFOAM/system/ChangeDictionary/executer.py
@@ -0,0 +1,7 @@
+from ..abstractSystemExecuter import abstractSystemExecuter
+
+class ChangeDictionary(abstractSystemExecuter):
+
+    def __init__(self,JSON):
+        super().__init__(JSON,"ChangeDictionary")
+
