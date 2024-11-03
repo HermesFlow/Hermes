fd78bda3bcbd99149e1cc3ea460823dc2c816cc2
Yehuda A <YA@i>
2024-11-03 14:59:54 +0200

    Changing the name of the solver from urban homogenous to homogenous

---
+++ hermes/Resources/openFOAM/constant/homogenousWindLogProfileDict/executer.py
@@ -0,0 +1,5 @@
+from ..abstractConstantExecuter import abstractConstantExecuter
+
+class homogenousWindLogProfileDict(abstractConstantExecuter):
+    def __init__(self,JSON):
+        super().__init__(JSON,"homogenousWindLogProfileDict")
