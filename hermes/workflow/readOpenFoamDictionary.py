9afbc4af1b4dedcd11ebafcc599f3f15b603680d
NogaObs <noga@iscfdc.com>
2025-07-14 16:19:26 +0300

    added export json

--- hermes/workflow/readOpenFoamDictionary.py
+++ hermes/workflow/readOpenFoamDictionary.py
@@ -3,6 +3,25 @@ from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
 from ..Resources.reTemplateCenter import templateCenter
 
 import copy
+import json
+from PyFoam.Basics.DataStructures import BoolProxy
+
+class FoamJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, BoolProxy):
+            return bool(o)
+        return super().default(o)
+
+def saveJson(data, saveName):
+    with open(saveName, "w", encoding="utf-8") as f:
+        json.dump(
+            data,
+            f,
+            indent=4,
+            sort_keys=True,
+            ensure_ascii=False,
+            cls=FoamJSONEncoder
+        )
 
 class readDictionary:
 
@@ -35,5 +54,8 @@ class readDictionary:
     def updateJsonFromDictionary(self):
         # upload JSON Templatte of the node
         node_type = "openFOAM.system.ControlDict"
-        fullNodeData = dict(templateCenter[node_type])
+        fullNodeData = dict(self._templateCenter[node_type])
         expand.updateMap(fullNodeData, self.dictData)
+
+
+
